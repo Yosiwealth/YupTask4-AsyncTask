5c88200d2b14fd91cb11c0ea98609bc37719b77f
Yosiwealth <adekanmbi.yosola@gmail.com>
2017-08-08 16:16:25 +0100

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,96 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.android.didyoufeelit;
+
+import android.*;
+import android.app.*;
+import android.os.*;
+import android.widget.*;
+
+/**
+ * Displays the perceived strength of a single earthquake event based on responses from people who
+ * felt the earthquake.
+ */
+public class MainActivity extends Activity {
+
+    /** URL for earthquake data from the USGS dataset */
+    private static final String USGS_REQUEST_URL =
+            "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=1990-01-01&endtime=1999-05-02&minfelt=50&minmagnitude=5";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+ 		// create an instance of the asynctask method (call earthquakeasyntask) //
+		EarthquakeAsyncTask task = new EarthquakeAsyncTask();
+		task.execute(USGS_REQUEST_URL);
+		
+		
+    }
+
+    /**
+     * Update the UI with the given earthquake information.
+     */
+    private void updateUi(Event earthquake) {
+        TextView titleTextView = (TextView) findViewById(R.id.title);
+        titleTextView.setText(earthquake.title);
+
+        TextView tsunamiTextView = (TextView) findViewById(R.id.number_of_people);
+        tsunamiTextView.setText(getString(R.string.num_people_felt_it, earthquake.numOfPeople));
+
+        TextView magnitudeTextView = (TextView) findViewById(R.id.perceived_magnitude);
+        magnitudeTextView.setText(earthquake.perceivedStrength);
+    }
+	
+	
+	
+	
+	
+	//					ASYNCTASK  				//
+	
+	
+	private class EarthquakeAsyncTask extends AsyncTask <String,Void,Event>
+	{
+
+		@Override
+		protected Event doInBackground(String[] urls)
+		{
+			// TODO: Implement this method
+			if (urls.length < 1 || urls[0] == null) {
+				return null;
+			}
+			Event result = Utils.fetchEarthquakeData(urls[0]);
+			return result;
+		}
+		
+
+		@Override
+		protected void onPostExecute(Event result)
+		{
+			// TODO: Implement this method
+			if (result == null) {
+				return;
+			}
+			updateUi(result);
+		}
+		
+		
+	}
+	
+	
+	
+}
